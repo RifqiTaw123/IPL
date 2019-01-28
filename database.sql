85a755d50cd7aba39fbe31b141efbd83cbc39432
dandibaroes <36442941+dandibaroes@users.noreply.github.com>
2019-01-28 23:24:47 +0700

    dml tabel Nasabah, tabel Akun, dan Tabel Admin

--- database.sql
+++ database.sql
@@ -61,26 +61,38 @@ CREATE TABLE Virtual_Akun (
 	CONSTRAINT Trans_vir FOREIGN KEY (No_Transaksi) REFERENCES Transaksi(No_Transaksi)
 );
 
-INSERT INTO Bank  (Kode_Bank, Nama_Bank, Alamat)
-VALUES ("B001","GangBank Bandung","Bandung"),
-	("B002","GangBank Jogjakarta","Jogjakarta"),
-	("B003","GangBank Jakarta","jakarta"),
-	("B004","GangBank Semarang","Semarang"),
-	("B005","GangBank Cilegon","Cilegon"),
-	("B006","GangBank Bali","Bali"),
-	("B007","GangBank Malang","Malang"),
-	("B008","GangBank Bogor","Bogor"),
-	("B009","GangBank Bekasi","Bekasi"),
-	("B010","GangBank Tanggerang","Tanggerang");
-	
-INSERT INTO Rekening  (No_Rekening, Kode_Bank, Saldo)
-VALUES ("666256111","B001",1000000),
-	("666256112","B002",1500000),
-	("666256113","B001",1000000000),
-	("666256114","B002",2000000),
-	("666256115","B003",3500000),
-	("666256116","B001",2360000),
-	("666256117","B005",7500000),
-	("666256118","B008",10000000),
-	("666256119","B007",15000000),
-	("666256120","B010",500000);
+INSERT INTO Nasabah (Id_Nasabah, No_Rekening, Nama, No_HP, Alamat)
+VALUES  ("N671001","666256111","Lukman","08777123666","sukabirus"),
+        ("N671002","666256112","Dandi","087771234267","sukapura"),
+        ("N671003","666256113","Khairul","08777123454","bojongsoang"),
+        ("N671004","666256114","Fikhri","08777127866","dayeuhkolot"),
+        ("N671005","666256115","Rifqi","08777123655","sukapura"),
+        ("N671006","666256116","Taufik","08777111666","cicaheum"),
+        ("N671007","666256117","Siti","08777123897","cimahi"),
+        ("N671008","666256118","Diah","08777153666","lembang"),
+        ("N671009","666256119","Brenda","08777120886","buah batu"),
+        ("N671010","666256120","Amel","08777123098","citeureup");
+
+INSERT INTO Akun (Id_Nasabah, Username, Password)
+VALUES  ("N671001","Lukman123","sandilukman"),
+        ("N671002","Dandi123","sandidandi"),
+        ("N671003","Khairul123","sandikhairul"),
+        ("N671004","Fikhri123","sandifikhri"),
+        ("N671005","Rifqi123","sandirifqi"),
+        ("N671006","Taufik123","sanditaufik"),
+        ("N671007","Siti123","sandisiti"),
+        ("N671008","Diah123","sandidiah"),
+        ("N671009","Brenda123","sandibrenda"),
+        ("N671010","Amel123","sandiamel");
+
+INSERT INTO Admin (Id_Admin, Nama_Admin, Alamat)
+VALUES ("A001","admin 1","bandung"),
+	("A002","admin 2","jakarta"),
+	("A003","admin 3","surabaya"),
+	("A004","admin 4","Jogjakarta"),
+	("A005","admin 5","Bekasi"),
+	("A006","admin 6","Depok"),
+	("A007","admin 7","Semarang"),
+	("A008","admin 8","Padang"),
+	("A009","admin 9","Aceh"),
+	("A010","admin 10","Bengkulu");
